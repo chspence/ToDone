6d6fb7d377b516da32c16b2269b4ef2d08b1dce7
DeveloperDemetri <developerdemetri@gmail.com>
2016-09-03 02:33:20 -0700

    log in route finished

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,93 @@ let bcrypt = require('bcrypt-nodejs');
 let redis_tool = require('../bin/redis_tool');
 let session_tool = require('../bin/session_tool');
 
+let uname_re = /^(\w{3,63})$/;
+
 let router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('home');
+  if (req.session.username) {
+    if (uname_re.test(req.session.username)) {
+      res.render('home');
+    }
+    else {
+      console.log('Possible spoofed session username: ', req.session.username);
+      req.session.destroy();
+      res.render('lost');
+    }
+  }
+  else {
+    res.render('login');
+  }
 });
 
-router.get('/hash', function(req, res) {
-  let hash = bcrypt.hashSync("myPasf8897987*@!word");
-  console.log(hash);
-  console.log(bcrypt.compareSync("bacon", hash)); // true
-  console.log(bcrypt.compareSync("veggies", hash)); // false
-  res.send(hash);
+router.post('/login', function(req, res) {
+  if (req.body.username && req.body.password) {
+    let pass_re = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9!@#$%^&*/._+-]{8,31})$/;
+    if (uname_re.test(req.body.username) && pass_re.test(req.body.password)) {
+      db_pool.connect(function(err, client, done) {
+        if (err) {
+          console.log('Error fetching client from pool: ', err);
+          let result = {
+            "status": 500,
+            "error": 'Database error.'
+          };
+          res.send(result);
+        }
+        else {
+          let uname = req.body.username.toLowerCase();
+          client.query('SELECT password FROM public.td_user WHERE username=$1', [uname], function(err, result) {
+            done();
+            if (err) {
+              console.log("Error fetching password for log in: ", err);
+              let result = {
+                "status": 500,
+                "error": 'Database error.'
+              };
+              res.send(result);
+            }
+            else {
+              if(result.rows[0] && bcrypt.compareSync(req.body.password, result.rows[0].password)) {
+                req.session.username = uname;
+                let result = {
+                  "status": 200,
+                  "message": 'Log in succeeded.'
+                };
+                res.send(result);
+              }
+              else {
+                let result = {
+                  "status": 200,
+                  "message": 'Log in failed.'
+                };
+                res.send(result);
+              }
+            }
+          });
+        }
+      });
+    }
+    else {
+      let result = {
+        "status": 400,
+        "error": 'Invalid username and/or password.'
+      };
+      res.send(result);
+    }
+  }
+  else {
+    let result = {
+      "status": 400,
+      "error": 'Missing username and/or password.'
+    };
+    res.send(result);
+  }
 });
 
 module.exports = router;
 
 /*
-Copyright 2016 DeveloperDemetri
+Copyright 2016 DeveloperDemetri and chspence
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
